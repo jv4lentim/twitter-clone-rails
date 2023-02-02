68007c711b71500bc0c483aeec14187292ae7f7e
João Victor <joaovictorvalentim@gmail.com>
2023-02-01 21:27:40 -0300

    clean up and tests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,16 @@
 class TweetsController < ApplicationController
-    def create
-        tweet = Tweet.new(tweet_params.merge(user: current_user))
+	before_action :authenticate_user!
+	def create
+		tweet = Tweet.new(tweet_params.merge(user: current_user))
 
-        if tweet.save
-            redirect_to root_path
-        end
-    end
+		if tweet.save
+				redirect_to dashboard_path
+		end
+	end
 
-    private
+	private
 
-    def tweet_params
-        params.require(:tweet).permit(:body)
-    end
+	def tweet_params
+		params.require(:tweet).permit(:body)
+	end
 end
